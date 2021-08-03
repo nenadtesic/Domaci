a5987b1043b73243d11df922d84e7b59f6bb2c53
nenadtesic <nndtesic@gmail.com>
2021-08-03 04:58:56 +0200

    radim domaci

--- Nedelja_2/Domaci/Sreda/script.js
+++ Nedelja_2/Domaci/Sreda/script.js
@@ -16,18 +16,26 @@ Puz se popeo na drvo za 8 dana
 
 Smatrati da drvo svakog dana raste fiksno 1cm. */
 let puz = 0;
-let drvo = 100;
-if (puz < drvo) {
-    while (puz < drvo) {
-        
+let drvo = 11;
+let dana = 0;
+
+while (puz < drvo) {
+    if (puz < drvo) {
+        puz += 3;
+        drvo++
+        dana++
+        if (puz > drvo) {
+            puz = drvo;
+            console.log(`Dan ${dana}: Puz je presao ${puz}cm, visina drveta je ${drvo}cm`);
+        } else {
+            console.log(`Dan ${dana}: Puz je presao ${puz}cm, visina drveta je ${drvo}cm`);
+        }
     }
 }
+console.log(`Puz se popeo na drvo za ${dana} dana`);
 
 
-
-
-
-/* 2. Treba izracunati poptust U DINARIMA koji ostvaruje kupac.
+/* 2. Treba izracunati popust U DINARIMA koji ostvaruje kupac.
 
 Pravila za ostvarivanje popusta su sledeca:
 
@@ -48,3 +56,52 @@ dakle racuna se 5% od 8000 din a ne od pocetnog iznosa
 i dobije se dodatni popust od 400.
 Tako da je na kraju ukupan popust 2000 + 400 = 2400 dinara
 */
+let iznos = 2200;
+let kupac = 'penzioner';
+let vikend 
+switch (kupac) {
+    case 'zaposlen':
+        if (iznos > 5000) {
+            popust = iznos - (iznos * 0.05);
+        } else {
+            popust = iznos;
+        }
+        console.log(popust);
+        break;
+
+    case 'student':
+        if (iznos > 3000) {
+            popust = iznos - (iznos * 0.2);
+        } else {
+            popust = iznos;
+        }
+        console.log(popust);
+        break;
+
+    case 'penzioner':
+        if (iznos > 2000) {
+            popust = iznos - (iznos * 0.3);
+        } else {
+            popust = iznos;
+        }
+        console.log(popust);
+        break;
+
+    case 'firma':
+        if (iznos > 10000) {
+            popust = iznos - (iznos * 0.1);
+        } else {
+            popust = iznos;
+        }
+        console.log(popust);
+        break;
+
+    case 'vikend':
+        popust = popust - (popust * 0.05);
+        console.log(popust);
+
+    default:
+        iznos = iznos;
+        console.log(popust);
+        break;
+}
