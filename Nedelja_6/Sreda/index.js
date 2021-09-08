19d7adb7000ff079301f6d145dc86be7ca6c3e8f
nenadtesic <nndtesic@gmail.com>
2021-09-08 19:12:27 +0200

    radim

--- Nedelja_6/Sreda/index.js
+++ Nedelja_6/Sreda/index.js
@@ -1,3 +1,26 @@
+// ## Prodavnica
+
+
+// 1. Korisnik unosi username i password i na klik Login dugmeta se kreira objekat klase Kupac.
+// Klasa kupac sadrzi ime ,sifru i korpu .
+// Div korpa se prikazuje tek kada se korisnik ulogovao.
+
+// 2. Na stranici su sve vreme izlistani svi dostupni proizvodi iz niza proizvoda kreiranih klasom Proizvod
+
+// jedan proizvod treba da sadrzi naziv , cenu , dostupnu kolicinu 
+
+// proizvod moze biti prehrambeni ili bela tehnika
+
+// ako je bela tehnika ima i garanciju
+
+// ako je prehrambeni ima rok trajanja
+
+// 3. Korisnik moze da doda proizvod u korpu , ako unese kolicinu vecu od dostupne kolicine ispisati poruku o gresci 
+// Proizvod iz korpe moze da se obrise klikom na dugme Obrisi iz korpe
+
+// DODATNO , NE MORA:
+// 4. Ako je korisnik admin moze da doda nov proizvod
+
 let registrovaniKupac = [new Kupac('Pera', '123'), new Kupac('Mika', 'abc')]
 
 let k = new Kupac(inputUsername.value, inputPassword.value)
